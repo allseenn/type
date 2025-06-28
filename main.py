447f79d6c7b1da8c5a44fb1da602d764c401472b
allseenn <allseen@live.ru>
2025-06-28 11:37:32 +0300

    +.gitignore

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-import os
+import typer
 
-name = os.getenv("MY_NAME", "Slava")
-print(f"Hello {name}!")
+def main(name: str, lastname: str = "", formal: bool = False):
+    """
+    Say hi to NAME, optionally with a --lastname.
+
+    If --formal is used, say hi very formally.
+    """
+
+    if formal:
+        print(f"Hello Ms. {name} {lastname}")
+    else:
+        print(f"Hi {name} {lastname}")
+
+if __name__ == "__main__":
+    typer.run(main)
